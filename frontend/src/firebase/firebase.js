f402347b1cf3b492e01cbad9ab81a7b0b04aaf23
Harsh <harshvardhanpatil707@gmail.com>
2025-02-19 18:27:42 +0530

    Final Commit

--- frontend/src/firebase/firebase.js
+++ frontend/src/firebase/firebase.js
@@ -8,13 +8,13 @@ import {getAuth,GoogleAuthProvider} from 'firebase/auth'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA1KqL_6dZxdSrmne8-lEktLjAauc3JoBo",
-  authDomain: "internarea-9f3ee.firebaseapp.com",
-  projectId: "internarea-9f3ee",
-  storageBucket: "internarea-9f3ee.appspot.com",
-  messagingSenderId: "394243308686",
-  appId: "1:394243308686:web:deeb5322eaf1651c321ddf",
-  measurementId: "G-WLZZD3TMX8"
+  apiKey: "AIzaSyBMQLR_snoIXmfUGlYwFD0LV0UScJBAKo4",
+  authDomain: "internarea-193f6.firebaseapp.com",
+  projectId: "internarea-193f6",
+  storageBucket: "internarea-193f6.firebasestorage.app",
+  messagingSenderId: "186401079193",
+  appId: "1:186401079193:web:5ef8f53964a572b39a5d08",
+  measurementId: "G-3ENDJFG9CP"
 };
 
 // Initialize Firebase
